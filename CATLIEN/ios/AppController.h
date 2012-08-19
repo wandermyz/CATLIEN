25d302b7be521579ac83c55f4c5231e5934371d4
Wander <lywander@gmail.com>
2012-08-18 17:18:29 -0700

    add project

---
+++ CATLIEN/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  CATLIENAppController.h
+//  CATLIEN
+//
+//  Created by Wander on 8/18/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController	*viewController;
+}
+
+@end
+
---
+++ mklink.sh
@@ -0,0 +1,4 @@
+ln -s ./modules/cocos2dx/cocos2dx ./CATLIEN/libs/cocos2dx
+mkdir ./CATLIEN/libs/CocosDenshion
+ls -s ./modules/cocos2dx/CocosDenshion/include ./CATLIEN/libs/CocosDenshion/include
+ls -s ./modules/cocos2dx/CocosDenshion/ios ./CATLIEN/libs/CocosDenshion/iphone
