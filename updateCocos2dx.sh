1e85be2b326ea4a615b47c72e797f1a0a42339df
Wander <lywander@gmail.com>
2012-08-18 18:44:52 -0700

    remove sh

--- updateCocos2dx.sh
+++
@@ -1,6 +0,0 @@
-cd ./CATLIEN/libs
-ln -s ../../modules/cocos2dx/cocos2dx ./cocos2dx
-mkdir ./CocosDenshion
-cd ./CocosDenshion
-ln -s ../../../modules/cocos2dx/CocosDenshion/include ./include
-ln -s ../../../modules/cocos2dx/CocosDenshion/ios ./iphone
